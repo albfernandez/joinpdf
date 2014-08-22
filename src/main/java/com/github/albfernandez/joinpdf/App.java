3e32feac4c9445f831c6f7e70c2f9d2618913089
alberto <infjaf@gmail.com>
2014-08-23 00:56:57 +0200

    Prepara el programa de linea de comandos

--- src/main/java/com/github/albfernandez/joinpdf/App.java
+++ src/main/java/com/github/albfernandez/joinpdf/App.java
@@ -1,11 +1,20 @@
 package com.github.albfernandez.joinpdf;
 
+import java.io.File;
+
 /**
- * Hello world!
  *
  */
 public class App {
-    public static void main( String[] args ) {
-        System.out.println( "Hello World!" );
+    public static void main( String[] args ) throws Exception {
+    	if (args.length < 2) {
+    		System.out.println("Debe indicar archivos");
+    		return;
+    	}
+    	JoinPdf join = new JoinPdf();
+    	for (int i = 0; i <= args.length -2; i++){
+    		join.addFile(new File(args[i]));
+    	}
+    	join.export(new File(args[args.length-1]));
     }
 }
